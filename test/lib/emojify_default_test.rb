c8465130d70f2e0a46ce3eecba2bff6336a936a7
Robert Evans <robert@codewranglers.org>
2012-09-24 10:48:57 -0700

    Passing Tests

--- test/lib/emojify_default_test.rb
+++ test/lib/emojify_default_test.rb
@@ -7,15 +7,15 @@ class EmojifyDefaultTest < MiniTest::Unit::TestCase
   include Emojify
 
   def test_replacing_keyword
-    assert_equal "Keywords <image src='images/emojis/smile.png' height='20' width='20'/>", emojify("Keywords :smile:")
+    assert_equal "Keywords <image src='/assets/emojis/smile.png' height='20' width='20'/>", emojify("Keywords :smile:")
   end
 
   def test_replacing_with_numbers
-    assert_equal "Keywords <image src='images/emojis/+1.png' height='20' width='20'/>", emojify("Keywords :+1:")
+    assert_equal "Keywords <image src='/assets/emojis/+1.png' height='20' width='20'/>", emojify("Keywords :+1:")
   end
 
   def test_replacing_multiple_words
-    assert_equal "Keywords <image src='images/emojis/smile.png' height='20' width='20'/> when <image src='images/emojis/rain.png' height='20' width='20'/> and <image src='images/emojis/laughter.png' height='20' width='20'/> today",
+    assert_equal "Keywords <image src='/assets/emojis/smile.png' height='20' width='20'/> when <image src='/assets/emojis/rain.png' height='20' width='20'/> and <image src='/assets/emojis/laughter.png' height='20' width='20'/> today",
       emojify("Keywords :smile: when :rain: and :laughter: today")
   end
 end
